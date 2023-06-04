3c36b214078b9b335361a51f83cb7966aa87e19e
Avngrss <97980284+Avngrss@users.noreply.github.com>
2023-06-04 21:29:16 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import "./index.scss";
 
@@ -8,7 +8,7 @@ import App from "./components/App/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
