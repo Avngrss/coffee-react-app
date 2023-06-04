d9bfa0ae6c31048ce0e4badac27ff7d54229408e
Avngrss <97980284+Avngrss@users.noreply.github.com>
2023-06-04 19:42:53 +0300

    finish coffee page render

--- src/pages/AboutCoffee.jsx
+++ src/pages/AboutCoffee.jsx
@@ -1,10 +1,31 @@
 import React from "react";
+
+import styles from "./aboutcoffee.module.scss";
+
 import Coffeeheader from "../components/Coffeeheader/Coffeeheader";
+import coffeePage from "../assets/img/coffeepage.jpg";
+import decorate from "../assets/img/beanslogos.svg";
 
 function AboutCoffee() {
   return (
     <div>
       <Coffeeheader />
+      <div className={styles.coffeeflex}>
+        <img src={coffeePage} alt="coffeePage" />
+        <div className={styles.left}>
+          <h2 className={styles.title}>About it</h2>
+          <img src={decorate} alt="decorate" />
+          <p>
+            <span className={styles.boldSpan}>Country: Brasil</span>
+          </p>
+          <p className={styles.text}>
+            <span className={styles.boldSpan}>Description:</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+          <p className={styles.text}>
+            Price: <span className={styles.price}>16.99$</span>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
