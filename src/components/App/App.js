809fcca628ee1a4fe4de6be84e3577ec6f125fdb
Avngrss <97980284+Avngrss@users.noreply.github.com>
2023-06-04 21:40:06 +0300

    dd

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import AnimateRoute from "../AnimateRoute/AnimateRoute";
 import Footer from "../Footer/Footer";
+import Main from "../Main/Main";
 import "./App.scss";
 
 function App() {
   return (
     <div className="container">
       <AnimateRoute />
+      <Main />
       <Footer />
     </div>
   );
