e9c8617f40db427356f69593562f9a2891f769e7
Avngrss <97980284+Avngrss@users.noreply.github.com>
2023-06-04 22:11:22 +0300

    fix img

--- src/pages/Pleasure.jsx
+++ src/pages/Pleasure.jsx
@@ -13,12 +13,12 @@ const title = "About our goods";
 const text =
   "Extremity sweetness difficult behaviour he of. On disposal of as landlord horrible.Afraid at highly months do things on at. Situation recommend objection do intentionso questions. As greatly removed calling pleased improve an. Last ask him cold feelmet spot shy want. Children me laughing we prospect answered followed. At it wentis song that held help face.";
 const coffee = [
-  { img: "/img/coffee1.png", title: "AROMISTICO Coffee 1 kg", country: "Brazil", price: "8.99$" },
-  { img: "/img/coffee2.png", title: "Uniflex Coffee 1 kg", country: "Kenya", price: "12$" },
-  { img: "/img/coffee3.png", title: "Drip Coffee 1 kg", country: "Columbia", price: "6.99$" },
-  { img: "/img/coffee4.jpg", title: "Aromika Coffee 1 kg", country: "Brazil", price: "9.99$" },
-  { img: "/img/coffee5.jpg", title: "Josse Coffee 1 kg", country: "Columbia", price: "17$" },
-  { img: "/img/coffee6.webp", title: "Deep Coffee 1 kg", country: "Kenya", price: "6.94$" },
+  { img: "img/coffee1.png", title: "AROMISTICO Coffee 1 kg", country: "Brazil", price: "8.99$" },
+  { img: "img/coffee2.png", title: "Uniflex Coffee 1 kg", country: "Kenya", price: "12$" },
+  { img: "img/coffee3.png", title: "Drip Coffee 1 kg", country: "Columbia", price: "6.99$" },
+  { img: "img/coffee4.jpg", title: "Aromika Coffee 1 kg", country: "Brazil", price: "9.99$" },
+  { img: "img/coffee5.jpg", title: "Josse Coffee 1 kg", country: "Columbia", price: "17$" },
+  { img: "img/coffee6.webp", title: "Deep Coffee 1 kg", country: "Kenya", price: "6.94$" },
 ];
 function Pleasure() {
   return (
